eedc121d1f33bdb41f2ad4de2ac8ddfab4cf51a1
Bracha <bdsites18@gmail.com>
2021-06-08 23:25:19 +0300

    Update files 9Jun2021

---
+++ js_19.js
@@ -0,0 +1,9 @@
+var myFamily =
+{
+  name: "Reuven",
+  age: 18,
+  favoriteFood: "pizza",
+  canDrive: false,
+};
+
+console.log(myFamily);
---
+++ js_20.js
@@ -0,0 +1,35 @@
+alert("Just to make sure the page is refreshing!");
+// USING A FOR LOOP TO PULL OUT ELEMENTS IN AN ARRAY
+
+// var family = ["Reuven", "Shimon", "Levy", "Yehuda", "Yissachar", "Zevulun"];
+//
+// for (var counter = 0; counter < family.length; counter++)
+//   {
+//     (family[counter]);
+//   }
+//
+// console.log(family);
+
+
+
+// USING A FOR LOOP ON NUNBERS
+//
+// for (var counter = 5; counter < 20; counter++)
+//   {
+//       "This is an odd number under 20: " + counter;
+//   };
+//
+
+
+  for (i = 0; i < 5; i++) {
+    console.log("The number is " + i);
+  }
+
+
+//
+//
+//
+//
+// for (var i = 0; i < array.length; i++) {
+//   array[i]
+// }
